0ba22c16cba113368ad6fb3162ac59302efd37f1
Rasmus Selsmark <rasmus@unity3d.com>
2023-02-12 10:54:59 +0100

    Created GuessANumber game

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿// opret instans af vores spil klasse
+var game = new GuessANumberGame();
+
+// og start spillet
+game.Start();
+
+
+/// <summary>
+/// Vores C# klasse som indeholder gæt-et-tal spillet
+/// </summary>
+public class GuessANumberGame
+{
+    // her definerer vi maximum for tallet der skal gættes
+    private const int Max = 100;
+
+    // tallet vi skal gætte
+    private int secretNumber;
+
+    public void Start()
+    {
+        WriteWelcomeMessage();
+        FindSecretNumber();
+
+        int guess;
+        do
+        {
+            guess = GetUserGuess();
+
+            if (guess < secretNumber)
+            {
+                Console.WriteLine("Du gættede for lavt, prøv igen.");
+            }
+            else if (guess > secretNumber)
+            {
+                Console.WriteLine("Du gættede for højt, prøv igen.");
+            }
+            Console.WriteLine();
+        } while (guess != secretNumber);
+
+        Console.WriteLine("Du gættede tallet, flot klaret!");
+    }
+
+    /// <summary>
+    /// Skriv en velkomstbesked til spilleren.
+    /// </summary>
+    private void WriteWelcomeMessage()
+    {
+        Console.WriteLine($"Hej, jeg tænker på et tal mellem 1 og {Max}, prøv at gætte det!");
+        Console.WriteLine();
+    }
+
+    /// <summary>
+    /// Find det hemmelige tal, som spilleren skal gætte.
+    /// </summary>
+    private void FindSecretNumber()
+    {
+        secretNumber = new Random().Next(Max);
+    }
+
+    /// <summary>
+    /// Lad spilleren indtaste gæt. Fortsætter indtil man har indtastet et heltal.
+    /// </summary>
+    /// <returns>Heltal med brugerens gæt</returns>
+    private int GetUserGuess()
+    {
+        while (true)
+        {
+            Console.Write("Indtast dit gæt: ");
+            var input = Console.ReadLine();
+
+            // check at vi indtastede integer / heltal
+            if (int.TryParse(input, out int guess))
+            {
+                return guess;
+            }
+
+            Console.WriteLine("Du skal indtaste et heltal.");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# GuessANumber
+# Gæt et tal
+
+(Example project in Danish for teaching C# fundamentals, used as part of Danish non-profit organization [Coding Pirates](https://codingpirates.dk/in-english))
+
+Simpelt eksempel projekt for introduktion til C#
+
+## Påkrævede værktøjer
+
+Enten
+
+* Visual Studio Community: https://visualstudio.microsoft.com/vs/community
+
+eller
+
+* .NET SDK: https://dotnet.microsoft.com/en-us/download/dotnet/7.0
+* F.eks. Visual Studio Code: https://code.visualstudio.com/download
+
+## Kom i gang
+
+Visual Studio Community:
+
+ * Opret et nyt C# projekt
+
+Visual Studio Code:
+
+* Kør følgende fra kommandolinje:
+  ```
+  mkdir GuessANumber
+  cd GuessANumber
+  dotnet new console
+  ```
+
+Du kan nu åbne `Program.cs` filen i Visual Studio Code
+
+## Kør spillet
+
+Visual Studio Community:
+
+* Tryk F5 eller afspil knappen
+
+Visual Studio Code:
+
+* Kør følgende fra kommandolinje
+  ```
+  dotnet run
+  ```
+
+## Fremgangsmåde
+
+1. Opret projektet og kør. Skulle gerne skrive "Hello, World!"
+1. Implementer `GuessANumberGame` klasse og `WriteWelcomeMessage()` metode
+   - Forklar lidt om klasser
+1. `FindSecretNumber()` metode, med tilhørende `secretNumber` variabel og `Max` konstant
+   - Forklar om metoder, variable og scope
+1. `GetUserGuess()` metode
+   - Metode der returnerer værdi
+   - Skriv værdien ud, inden vi går videre
+1. Resten af programmet, med `do {} while` løkke indtil man har gættet tallet.
